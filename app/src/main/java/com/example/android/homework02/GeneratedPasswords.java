5981fd46c3ac0de40678fad2b678cd87121a68ba
Shikha <sbhatt10@uncc.edu>
2019-02-19 15:52:23 -0500

    Update Thread

--- app/src/main/java/com/example/android/homework02/GeneratedPasswords.java
+++ app/src/main/java/com/example/android/homework02/GeneratedPasswords.java
@@ -1,36 +1,21 @@
 package com.example.android.homework02;
 
 
-import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.ScrollView;
-import android.widget.TextView;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.ScrollView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.List;
 
-public class GeneratedPasswords extends AppCompatActivity {
+
+public class GeneratedPasswords extends MainActivity {
 
     ScrollView scrollView;
     LinearLayout linearLayout;
-
+    ArrayList <String> array = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,24 +23,9 @@ public class GeneratedPasswords extends AppCompatActivity {
         setContentView(R.layout.activity_generated_passwords);
         scrollView = (ScrollView) findViewById(R.id.asyncScrollView);
 
+    }
 
-
-//        ArrayList <String> array = new ArrayList<>();
-//        linearLayout = findViewById(R.id.threadLinearLayout);
-//        LayoutInflater inflater = LayoutInflater.from(this);
-//        TextView textView;
-//
-//        array = getIntent().getExtras().getStringArrayList(MainActivity.ARRAY_KEY);
-//        for(int x=0; x<array.size(); x++){
-//            View view =  inflater.inflate(R.layout.scrollview_text, linearLayout, false);
-//
-//            textView = view.findViewById(R.id.threadTextView);
-//
-//            textView.setText(array.get(x));
-//            linearLayout.addView(view);
-//
-//        }addView
-
+    private void displayUsingAsync() {
 
     }
 }
